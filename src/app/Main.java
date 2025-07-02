002131896549f46c8d60899cfe4828ffca5d2cb8
Petro_Krupko <bmxgtnz@gmail.com>
2025-07-02 19:16:10 +0200

    Create converter

---
+++ src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Main {
+
+    private static final double CONV_k = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter app");
+
+        double kgs = 5;
+        double pounds = convKgsToPounds(kgs);
+        System.out.println("Result is " + pounds + " pounds. ");
+    }
+
+    private static double convKgsToPounds(double kgs){
+        return kgs * CONV_k;
+    }
+}
