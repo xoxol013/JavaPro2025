bfdee90103c732adf7b133699e32574d42bb61a1
Petro_Krupko <38090405+xoxol013@users.noreply.github.com>
2025-08-06 02:12:10 +0200

    create converter kilometers in milies

--- src/hm_2/Main.java
+++ src/hm_2/Main.java
@@ -7,5 +7,9 @@ public class Main {
         double kilometers = milies * 1.60934;
         System.out.println(milies + " milies = " + kilometers + " kilometers");
 
+        double kilometersTwo = 15;
+        double miliesTwo = kilometersTwo * 0.621371;
+        System.out.println(kilometersTwo + " kilometers = " + miliesTwo + " milies");
+
     }
 }
