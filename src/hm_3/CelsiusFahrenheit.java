6f8c9db3d73c1a51d45775bac255b58ca97b4a12
Petro_Krupko <38090405+xoxol013@users.noreply.github.com>
2025-08-07 00:50:55 +0200

    ready converter celsius in fahrenheit

--- src/hm_3/CelsiusFahrenheit.java
+++ src/hm_3/CelsiusFahrenheit.java
@@ -3,5 +3,9 @@ package hm_3;
 public class CelsiusFahrenheit {
     public static void main(String[] args) {
 
+        // Create converter fahrenheits in celsius
+        double fahrenheits = 44;
+        double celsius = (fahrenheits - 32) * 5 / 9;
+        System.out.println(fahrenheits + " fahrenheits = " + celsius + " celsius");
     }
 }
