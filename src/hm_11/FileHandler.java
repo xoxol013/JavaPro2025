ca48c05cb603a5ebe8028d180dddd8a4fcb8389f
Petro_Krupko <38090405+xoxol013@users.noreply.github.com>
2025-10-27 12:53:39 +0100

    file improvement

---
+++ src/hm_11/FileHandler.java
@@ -0,0 +1,40 @@
+package hm_11;
+
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileHandler {
+
+    public String createFile(String path) {
+        Path newFile;
+        try {
+            newFile = Files.createFile(Path.of(path));
+        } catch (FileAlreadyExistsException e) {
+            return "File already exists!";
+        } catch (IOException e) {
+            return "Something  wrong " + e.getMessage();
+        }
+        return "Created " + newFile;
+    }
+
+    public String writeToFile(Path path, String content) {
+        try {
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        return "Recorded in " + path;
+    }
+
+    public String readFromFile(String path) {
+        try {
+            return Files.readString(Path.of(path));
+        } catch (IOException e) {
+            return "Something wrong " + e.getMessage();
+        }
+    }
+
+}
---
+++ src/hm_11/Main.java
@@ -0,0 +1,23 @@
+package hm_11;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    private final static String BASE_PATH = "files/";
+
+    public static void main(String[] args) {
+        FileHandler handler = new FileHandler();
+        String newFileName = "myFile";
+        String content = "Super information.";
+        String path = BASE_PATH + newFileName + ".txt";
+        getOutput(handler.createFile(path));
+        getOutput(handler.writeToFile(Paths.get(path), content));
+        getOutput("CONTENT: " + handler.readFromFile(path));
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
