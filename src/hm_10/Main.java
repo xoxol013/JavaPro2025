3e5735e156ba5a853bfa567e2830cb4ede07f8e3
Petro_Krupko <38090405+xoxol013@users.noreply.github.com>
2025-10-27 12:19:23 +0100

    work with files

---
+++ src/hm_10/Main.java
@@ -0,0 +1,20 @@
+package hm_10;
+
+public class Main {
+
+    private final static String BASE_PATH = "files/";
+
+    public static void main(String[] args) {
+        FileHandler handler = new FileHandler();
+        String fileName = "myFile";
+        String fileContent = "My very important information.";
+        String result = handler.writeFile(fileName, fileContent);
+        String content = handler.readFile(BASE_PATH + fileName + ".txt");
+        getOutput("RESULT: " + result);
+        getOutput("FILE CONTENT: " + content);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
