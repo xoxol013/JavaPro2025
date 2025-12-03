dc11a61a9b651f42638ae1d56ba6f108e56c7066
Petro_Krupko <38090405+xoxol013@users.noreply.github.com>
2025-12-03 20:55:52 +0100

    log

---
+++ src/hm_24/Logger.java
@@ -0,0 +1,28 @@
+package hm_24;
+
+public class Logger {
+
+    private static Logger instance;
+
+    private StringBuilder logs = new StringBuilder();
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        logs.append(message).append("\n");
+    }
+
+    public void printLogs() {
+        System.out.println("--- LOGS ---");
+        System.out.println(logs.toString());
+    }
+}
+
---
+++ src/hm_24/Main.java
@@ -0,0 +1,16 @@
+package hm_24;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First log");
+        logger2.log("Another log from a different place");
+
+        System.out.println(logger1 == logger2);
+
+        logger1.printLogs();
+    }
+}
